0f5bcd0f899472dcd8e17a728f6dc4a45a109562
Viraj-S45 <virajshirodkar45@gmail.com>
2025-07-02 16:34:28 +0530

    wlogout

--- home/modules/wlogout.nix
+++ home/modules/wlogout.nix
@@ -10,18 +10,6 @@
   programs.wlogout = {
     enable = true;
     layout = [
-      # {
-      #     "label" = "lock";
-      #     "action" = "hyprlock";
-      #     "text" = "Lock";
-      #     "keybind" = "l";
-      # }
-      # {
-      #     "label" = "hibernate";
-      #     "action" = "systemctl hibernate";
-      #     "text" = "Hibernate";
-      #     "keybind" = "h";
-      # }
       {
           "label" = "logout";
           "action" = "hyprctl dispatch exit";
@@ -34,60 +22,36 @@
           "text" = "Shutdown";
           "keybind" = "s";
       }
-      # {
-      #     "label" = "suspend";
-      #     "action" = "systemctl suspend";
-      #     "text" = "Suspend";
-      #     "keybind" = "u";
-      # }
       {
           "label" = "reboot";
           "action" = "systemctl reboot";
           "text" = "Reboot";
           "keybind" = "r";
       }
+      {
+          "label" = "suspend";
+          "action" = "systemctl suspend";
+          "text" = "Suspend";
+          "keybind" = "u";
+      }
+      {
+          "label" = "lock";
+          "action" = "hyprlock";
+          "text" = "Lock";
+          "keybind" = "l";
+      }
+      {
+          "label" = "hibernate";
+          "action" = "systemctl hibernate";
+          "text" = "Hibernate";
+          "keybind" = "h";
+      }
     ];
 #     style = '''';
-    style = ''
-            * {
-              background-image: none;
-            }
-
-            window {
-              background-color: rgba(0, 0, 0, 0.5);
-            }
-
-            button {
-              background-color: #2e3440;
-              border-style: solid;
-              border-width: 0px;
-              border-radius: 0;
-              background-repeat: no-repeat;
-              background-position: center;
-              background-size: 25%;
-              # color: #5e81ac;
-            }
-
-            button:focus, button:active, button:hover {
-              # background-color: #4c566a;
-              # color: #81a1c1;
-              outline-style: none;
-            }
-
-
-            #logout {
-              background-image: url("${./images/logout.png}");
-            }
-
-            #shutdown {
-              background-image: url("${./images/shutdown.png}");
-            }
-
-            #restart {
-              background-image: url("${./images/reboot.png}");
-            }
+ 
+ 
             
-          '';    
+ 
   };
 
 
